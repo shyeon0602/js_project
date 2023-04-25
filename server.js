af4701f82b6dad0e328bcc3343bc71a542318062
shyeon0602 <july8560@naver.com>
2023-04-25 22:42:59 +0900

    서버 css 연결

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+//홈 html파일 연결
 app.get("/", (req, res) => {
   // res.send("Hello World!");
   //__dirname: server.js 경로 불러오기
   res.sendFile(__dirname + "/main-page/main.html");
 });
+
+//css 적용
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
