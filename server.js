c35fa51e55e45e6630a997b8f3c037b1189d253d
shyeon0602 <july8560@naver.com>
2023-04-25 21:44:23 +0900

    nodemon 설치

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
+  //__dirname: server.js 경로 불러오기
+  res.sendFile(__dirname + "/main-page/main.html");
 });
 
 app.listen(3000, () => {
